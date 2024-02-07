12d153a68a936d4450b5d6bb55287120b0b33d28
Burak Temelkaya <burak.temelkaya@functionair.com>
2024-02-07 05:07:13 +0300

    Add project files.

---
+++ demoProjects/rentACar/Application/Features/Brands/Dtos/BrandGetByIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Brands.Dtos
+{
+    public class BrandGetByIdDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ demoProjects/rentACar/WebAPI/Program.cs
@@ -0,0 +1,41 @@
+using Persistence;
+using Application;
+using Core.CrossCuttingConcerns.Exceptions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddSecurityServices();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else if (app.Environment.IsProduction())
+{
+    app.ConfigureCustomExceptionMiddleware();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
